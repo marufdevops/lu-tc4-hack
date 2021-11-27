84a80f40e4d4f1ec7063e1042aeb6a939837683d
Siam Ahmed <fassterthanflash@gmail.com>
2021-11-27 12:38:37 +0600

    login added

--- frontend/src/Pages/Auth/Login/Login.jsx
+++ frontend/src/Pages/Auth/Login/Login.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import Topbar from '../../../Components/Topbar/Topbar'
+import { Radio, Button, TextField, RadioGroup, FormControlLabel, FormLabel } from '@mui/material'
+import styles from './Login.module.css'
+import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
+import axios from '../../../Helper/axios'
+import Cookies from 'universal-cookie';
+import { useHistory } from "react-router-dom"
+const cookies = new Cookies()
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [radio, setRadio] = useState("customer")
+  const formRef = React.useRef();
+  let history = useHistory();
+  const handleChange = (event, type) => {
+    switch (type) {
+      case "email":
+        setEmail(event.target.value)
+        break
+      case "password":
+        setPassword(event.target.value)
+        break
+      case "radio":
+        setRadio(event.target.value)
+        break
+      default:
+        break;
+    }
+  }
+
+  const submit = () => {
+    if (formRef.current.reportValidity) {
+      const body = {
+        email,
+        role: radio,
+        password
+      }
+      console.log(body);
+    //   axios.post("/api/users/login", body)
+    //     .then(res => {
+    //       cookies.set("bargainc", res.data.accessToken)
+    //       cookies.set("bargainr", body.role)
+    //       history.push("/home");
+    //     }).catch(err => {
+    //       alert('Incorrect Password!')
+    //       console.log(err);
+    //     })
+    }
+
+  }
+
+  useEffect(() => {
+    cookies.remove("bargainc")
+    cookies.remove("bargainr")
+    document.title = "Login - Bargain";
+  }, []);
+  return (
+    <div className={styles.mainDiv} style={{ backgroundColor: "#EFF5E9" }}>
+      <Topbar list={[]} />
+      <div className={styles.mainSignUpDiv}>
+        <form ref={formRef} className={styles.signUpDiv}>
+          <p className={styles.signUpText}>Log In</p>
+          <TextField required="true" onChange={(event) => { handleChange(event, "email") }} className={styles.textField} id="standard-basic" type="email" label="Email" variant="standard" />
+          <TextField required="true" onChange={(event) => { handleChange(event, "password") }} className={styles.textField} id="standard-basic" label="Passowrd" type="password" variant="standard" />
+
+          <div className={styles.radioDiv}>
+            <FormLabel className={styles.radioLabel} component="legend">Account Type</FormLabel>
+            <RadioGroup value={radio} onChange={(event) => { handleChange(event, "radio") }} row aria-label="gender" name="row-radio-buttons-group">
+              <FormControlLabel value="customer" control={<Radio />} label="Customer" />
+              <FormControlLabel value="seller" control={<Radio />} label="Seller" />
+            </RadioGroup>
+          </div>
+
+
+          <Button className={styles.btn} onClick={submit} variant="contained" endIcon={<DoubleArrowIcon />}>Submit</Button>
+          <p>Don't have an account? <a className={styles.signUpLink} href="/signUp">Sign Up</a> </p>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- frontend/src/Routes/RouteManager.jsx
+++ frontend/src/Routes/RouteManager.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useMemo } from 'react'
 import { Route, Switch } from "react-router-dom";
 import { UserContext } from '../Helper/userContext'
+import Login from '../Pages/Auth/Login/Login';
 import SignUp from '../Pages/Auth/SignUp/SignUp';
+import Verification from '../Pages/Auth/Verification/Verification';
 const RouteManager = () => {
     const [user, setUser] = useState(null);
 
     const value = useMemo(() => ({ user, setUser }), [user, setUser]);
     return (
         <Switch>
+            <Route path={"/login"} exact component={Login} />
             <UserContext.Provider value={value}>
-            <Route path={"/signUp"} exact component={SignUp} />
+                <Route path={"/signUp"} exact component={SignUp} />
+                <Route path={"/verification"} exact component={Verification} />
             </UserContext.Provider>
         </Switch>
     )
