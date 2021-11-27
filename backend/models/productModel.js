c0bf10f7f91d3946f06ab48e67b6b16541d67127
Tausif Ahmed <tausif@cuebites.com>
2021-11-27 14:16:25 +0600

    product schema implement

---
+++ backend/models/productModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+//Schema Creation
+const productSchema = new mongoose.Schema(
+  {
+    _sellerId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Seller",
+      required: [true, "A product must have a seller"],
+    },
+    productName: {
+      type: String,
+      required: [true, "Please provide a product name"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please select a category"],
+    },
+    startingBid: {
+      type: Number,
+      unique: true,
+      required: [true, "Please enter a lowest bid amount"],
+    },
+    auctionDeadline: {
+      type: String,
+      required: [true, "Please provide a deadline for the auction"],
+    },
+
+    winningCriteria: {
+      type: String,
+      required: [true],
+      default: "automatic"
+    },
+    winningCondition: {
+      type: String,
+      required: [true],
+      default: "highestBidder"
+    },
+    photos: [String],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//Model Creation
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
