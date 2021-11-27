7ad96e868ed541ab385cb1b40b016c1b3e8257e4
Tausif Ahmed <tausif@cuebites.com>
2021-11-27 12:30:14 +0600

    customer Profile Edit implemented

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const customerController = require("../controllers/customerController");
+
 
 const router = express.Router();
 
 //Auth routes
 router.post("/signup", authController.signUser);
 router.post("/login", authController.login);
+
+//All routes after this middleware are protected
+
+router.patch(
+  "/updateProfileInfo/consumer",
+  customerController.updateProfileInfo
+);
 
 module.exports = router;
