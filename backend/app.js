849044b9025f702f559ce814485f80f6b9a4df86
Sajidul Kabir <sajidulkabiradib@gmail.com>
2021-11-27 10:55:46 +0600

    initial backend boilerplate

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+// Cross-Origin Resource Sharing middleware
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PATCH"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("hsad");
+});
+
+module.exports = app;
